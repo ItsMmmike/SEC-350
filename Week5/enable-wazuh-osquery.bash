43ab447143dd41e0f4b7a07f9bff203d3f7aac1d
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-03-02 23:10:27 -0500

    Create enable-wazuh-osquery.bash

---
+++ Week5/enable-wazuh-osquery.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script used to configure the OSQuery service on a Rocky-Based System and Update the Wazuh-Agent Configuration to send osquery event logs to Wazuh Server
+# Run script as sudo user
+
+# Source Reference: https://documentation.wazuh.com/4.7/user-manual/capabilities/malware-detection/osquery.html#configuration
+
+# Install OS Query
+curl -L https://pkg.osquery.io/rpm/GPG | sudo tee /etc/pki/rpm-gpg/RPM-GPG-KEY-osquery
+sudo yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
+sudo yum-config-manager --enable osquery-s3-rpm-repo
+sudo yum install osquery -y
+
+# Add OSQuery Configuration Files and move to location
+curl -L https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week5/osquery.conf | sudo tee /etc/osquery/osquery.conf
+
+# Enable the Service
+sudo systemctl enable osqueryd
+#sudo systemctl start osqueryd
+
+# Update Wazuh-Agent Configuration File
+curl -L https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week5/wazuh-agent-osquery.conf | sudo tee /var/ossec/etc/ossec.conf
+
+# Restart Wazuh-Agent
+sudo systemctl restart wazuh-agent
