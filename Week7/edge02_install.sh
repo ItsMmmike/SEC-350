b332ed4f50411d78010b1b7a18ce1cf020e4977f
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-03-21 13:01:23 -0400

    Create edge02_install.sh working

---
+++ Week7/edge02_install.sh
@@ -0,0 +1,15 @@
+#!/bin/vbash
+# Script use to configure Vyos Firewall/Router for SEC-350
+
+# Need to source vyos script commands from this file below
+source /opt/vyatta/etc/functions/script-template
+
+# Configuration commands go here
+configure
+
+# Set configuration commands go here
+
+# Save Configuration
+commit
+exit
+
