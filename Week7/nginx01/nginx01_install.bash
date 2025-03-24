8096426b247c2d27b49ca32df108c3a4e985d57a
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-03-23 21:53:30 -0400

    Update nginx01_install.bash

--- Week7/nginx01/nginx01_install.bash
+++ Week7/nginx01/nginx01_install.bash
@@ -6,13 +6,13 @@ sudo apt update
 sudo apt install nginx -y
 
 # Download Nginx Config Files
-sudo wget -O <file> "https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week7/nginx01/nginx.conf"
-sudo wget -O <file> "https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week7/nginx01/index.html"
-sudo wget -O <file> "https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week7/nginx01/cool-image.png"
+sudo wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week7/nginx01/nginx.conf"
+sudo wget -O /var/www/html/index.html "https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week7/nginx01/index.html"
+sudo wget -O /var/www/html/cool-image.png "https://raw.githubusercontent.com/ItsMmmike/SEC-350/refs/heads/main/Week7/nginx01/cool-image.png"
 
 # Enable and Start server
 sudo systemctl enable nginx
-sudo systemctl start nginx
+sudo systemctl restart nginx
 
 # Set Hostname
 sudo hostnamectl set-hostname dhcp01-michael
