c8241bf3d23650bb9cf7906f611e7874f87579b6
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2025-04-16 15:53:11 -0400

    Update wireguard_install.bash

--- Week9/wireguard_install.bash
+++ Week9/wireguard_install.bash
@@ -51,9 +51,9 @@ PersistentKeepalive = 25
 EOF
 
 # Firewalld config
-sudo firewall-cmd --permanetn --zone=public --add-port=51820/udp
+sudo firewall-cmd --permanent --zone=public --add-port=51820/udp
 sudo firewall-cmd --permanent --add-interface=wg0 --zone=internal
-sudo firewall-cmd --permanetn --zone=internal --add-masquerade
+sudo firewall-cmd --permanent --zone=internal --add-masquerade
 sudo firewall-cmd --reload
 
 # Start Wireguard
